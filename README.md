623ea86e86f62e8a8df68512530486b8ebbb34a8
Andrei Neculau <andrei.neculau@klarna.com>
2013-04-01 12:40:24 +0200

    1.9 port

---
+++ README.md
@@ -0,0 +1,71 @@
+# Pidgin Original Smileys
+
+A Pidgin theme that implements original protocol smileys, originally hosted [here](http://wiki.andreineculau.com/Original_Smileys_Theme_for_Pidgin).
+
+![](pidgin-original/pidgin-original.png)
+
+# WARNINGs
+
+## Copyright/License
+
+Emoticons are gray territory. While they are re-used in several ways across the web, they are actually copyrighted under different license terms.
+
+This theme on the other hand is [unlicensed](http://unlicense.org/).
+
+## Contribution
+
+This Pidgin theme hasn't been under development since 2010-02-26.
+
+~~If~~ When you find missing/incorrect/low-quality smileys, please file [a Github issue](issues/new), or even better send a pull request.
+
+# Overview
+
+The birth: September 2007.
+
+The scope: [Pidgin](https://pidgin.im) (multi-protocol messenger).
+
+The innovation: use the original emoticons/smileys of their respective messenger/protocol - Yahoo!, AIM, ICQ, etc.
+
+The reasoning: if emoticons show emotions, then different drawings show different emotions, no matter how many similarities they share.
+
+The technical common-sense: emoticons have grown to be proprietary to the protocol. So it is common-sense that when you are using a certain protocol to communicate, you use its emoticons too.
+
+The e-collaboration principle: WYSIWIS (What-you-see-is-what-I-see).
+
+The hibernation: February 2010.
+
+The changelog: [pidgin-original-changelog.txt](pidgin-original-changelog.txt)
+
+# Supported protocols
+
+* [AIM](http://en.wikipedia.org/wiki/Aol_Instant_Messenger)
+* [Gadu-Gadu](http://en.wikipedia.org/wiki/Gadu-Gadu)
+* [Google Talk](http://en.wikipedia.org/wiki/Google_Talk)
+    * hidden emoticons (all visible)
+    * round emoticons
+    * square emoticons
+    * colored emoticons
+* [Facebook](http://en.wikipedia.org/wiki/Facebook) (requires the [Facebook Chat plugin for Pidgin](http://code.google.com/p/pidgin-facebookchat/))
+* [ICQ](http://en.wikipedia.org/wiki/ICQ)
+* [MSN/Live!/WLM](http://en.wikipedia.org/wiki/Windows_Live_Messenger)
+    * hidden emoticons (all visible)
+    * support for [MSN Pecan](http://code.google.com/p/msn-pecan/) plugin
+    * support for [Microsoft OCS/LCS](http://sipe.sourceforge.net/) plugin
+* [MySpaceIM](http://en.wikipedia.org/wiki/MySpaceIM)
+* [Sametime](http://en.wikipedia.org/wiki/IBM_Lotus_Sametime)
+* [Skype](http://en.wikipedia.org/wiki/Skype) (requires the [Skype plugin for Pidgin](http://eion.robbmob.com/))
+    * hidden emoticons (all visible, except the flags)
+    * support for D-Bus plugin
+* [Tencent QQ/Messenger](http://en.wikipedia.org/wiki/Tencent_QQ) (experimental; looking for feedback!)
+* [Tlen.pl](http://en.wikipedia.org/wiki/Tlen.pl) (thanks to Kasper Wieczorek)
+* [Yahoo!](http://en.wikipedia.org/wiki/Yahoo!_Messenger)
+    * hidden emoticons (all visible)
+    * big emoticons
+
+# Installation
+
+1. Download the latest [tgz](archives/v1.9.tgz)
+1. Open Pidgin, click Tools from the main menu, then Preferences (or simply Ctrl+P in the Pidgin window)
+1. Go to the Smiley Themes tab
+1. Drag and drop the downloaded tgz archive (i.e. pidgin-original-x.x.x.tgz), or click Add and point to the archive
+1. Select Original Smileys from the list, and click OK
---
+++ pidgin-original-qq/theme.cc
@@ -0,0 +1,113 @@
+Name=Original Smileys, QQ
+Description=QQ as default
+Icon=../pidgin-original/qq/0.gif
+Author=Andrei Neculau
+
+
+# Tencent QQ
+[default]
+../pidgin-original/qq/0.gif		微笑	/wx
+../pidgin-original/qq/1.gif		撇嘴	/pz
+../pidgin-original/qq/2.gif		色		/se
+../pidgin-original/qq/3.gif		发呆	/fd
+../pidgin-original/qq/4.gif		得意	/dy
+../pidgin-original/qq/5.gif		流泪	/ll
+../pidgin-original/qq/6.gif		害羞	/hx
+../pidgin-original/qq/7.gif		闭嘴	/bz
+../pidgin-original/qq/8.gif		睡		/shui
+../pidgin-original/qq/9.gif		大哭	/dk
+../pidgin-original/qq/10.gif	尴尬	/gg
+../pidgin-original/qq/11.gif	发怒	/fn
+../pidgin-original/qq/12.gif	调皮	/tp
+../pidgin-original/qq/13.gif	呲牙	/cy
+../pidgin-original/qq/14.gif	/jy
+../pidgin-original/qq/15.gif	难过	/ng
+../pidgin-original/qq/16.gif	酷		/kuk
+../pidgin-original/qq/17.gif	/lengh
+../pidgin-original/qq/18.gif	抓狂	/zk
+../pidgin-original/qq/19.gif	吐		/tu
+../pidgin-original/qq/20.gif	偷笑	/tx
+../pidgin-original/qq/21.gif	可爱	/ka
+../pidgin-original/qq/22.gif	白眼	/baiy
+../pidgin-original/qq/23.gif	傲慢	/am
+../pidgin-original/qq/24.gif	饥饿	/jie
+../pidgin-original/qq/25.gif	困		/kun
+../pidgin-original/qq/26.gif	惊恐	/jk
+../pidgin-original/qq/27.gif	流汗	/lh
+../pidgin-original/qq/28.gif	憨笑	/hanx
+../pidgin-original/qq/29.gif	大兵	/db
+../pidgin-original/qq/30.gif	奋斗	/fendou
+../pidgin-original/qq/31.gif	咒骂	/zhm
+../pidgin-original/qq/32.gif	疑问	/yiw
+../pidgin-original/qq/33.gif	嘘...	/xu
+../pidgin-original/qq/34.gif	晕		/yun
+../pidgin-original/qq/35.gif	折磨	/zhem
+../pidgin-original/qq/36.gif	衰		/shuai
+../pidgin-original/qq/37.gif	骷髅	/kl
+../pidgin-original/qq/38.gif	敲打	/qiao
+../pidgin-original/qq/39.gif	再见	/zj
+../pidgin-original/qq/40.gif	/ch
+../pidgin-original/qq/41.gif	/kb
+../pidgin-original/qq/42.gif	/gz
+../pidgin-original/qq/43.gif	/qd
+../pidgin-original/qq/44.gif	/huaix
+../pidgin-original/qq/45.gif	/zhh
+../pidgin-original/qq/46.gif	/yhh
+../pidgin-original/qq/47.gif	/hq
+../pidgin-original/qq/48.gif	/bs
+../pidgin-original/qq/49.gif	/wq
+../pidgin-original/qq/50.gif	/kk
+../pidgin-original/qq/51.gif	/yx
+../pidgin-original/qq/52.gif	/qq
+../pidgin-original/qq/53.gif	/xia
+../pidgin-original/qq/54.gif	/kel
+../pidgin-original/qq/55.gif	/cd
+../pidgin-original/qq/56.gif	西瓜	/xig
+../pidgin-original/qq/57.gif	/pj
+../pidgin-original/qq/58.gif	/lq
+../pidgin-original/qq/59.gif	/pp
+../pidgin-original/qq/60.gif	咖啡	/kf
+../pidgin-original/qq/61.gif	/fa
+../pidgin-original/qq/62.gif	猪头	/zt
+../pidgin-original/qq/63.gif	玫瑰	/mg
+../pidgin-original/qq/64.gif	凋谢	/dx
+../pidgin-original/qq/65.gif	/sa
+../pidgin-original/qq/66.gif	爱心	/xin
+../pidgin-original/qq/67.gif	心碎	/xs
+../pidgin-original/qq/68.gif	蛋糕	/dg
+../pidgin-original/qq/69.gif	闪电	/shd
+../pidgin-original/qq/70.gif	炸弹	/zhd
+../pidgin-original/qq/71.gif	刀		/dao
+../pidgin-original/qq/72.gif	足球	/zq
+../pidgin-original/qq/73.gif	/pch
+../pidgin-original/qq/74.gif	便便	/bb
+../pidgin-original/qq/75.gif	月亮	/yl
+../pidgin-original/qq/76.gif	太阳	/ty
+../pidgin-original/qq/77.gif	礼物	/lw
+../pidgin-original/qq/78.gif	拥抱	/yb
+../pidgin-original/qq/79.gif	/qiang
+../pidgin-original/qq/80.gif	弱		/ruo
+../pidgin-original/qq/81.gif	握手	/ws
+../pidgin-original/qq/82.gif	/v
+../pidgin-original/qq/83.gif	/bq
+../pidgin-original/qq/84.gif	/gy
+../pidgin-original/qq/85.gif	/qt
+../pidgin-original/qq/86.gif	/cj
+../pidgin-original/qq/87.gif	/aini
+../pidgin-original/qq/88.gif	/no
+../pidgin-original/qq/89.gif	/ok
+../pidgin-original/qq/90.gif	/love
+../pidgin-original/qq/91.gif	飞吻	/fw
+../pidgin-original/qq/92.gif	跳		/tiao
+../pidgin-original/qq/93.gif	发抖	/fad
+../pidgin-original/qq/94.gif	怄火	/oh
+../pidgin-original/qq/95.gif	/zhq
+../pidgin-original/qq/96.gif	/kt
+../pidgin-original/qq/97.gif	/turn
+../pidgin-original/qq/98.gif	/skip
+../pidgin-original/qq/99.gif	/hsh
+../pidgin-original/qq/100.gif	/jd
+../pidgin-original/qq/101.gif	/jw
+../pidgin-original/qq/102.gif	/xw
+../pidgin-original/qq/103.gif	/ztj
+../pidgin-original/qq/104.gif	/ytj
